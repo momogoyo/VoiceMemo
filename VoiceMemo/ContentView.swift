af714486ad81c3be70d239b66b7c4bf860d0f024
momogoyo <metheduxk@gmail.com>
2025-08-11 12:44:32 +0900

    Initial Commit

---
+++ VoiceMemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceMemo
+//
+//  Created by 현유진 on 8/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by 현유진 on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
