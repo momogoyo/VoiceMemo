93da8b377c4b2493331211c167e5cffc445eb663
momogoyo <metheduxk@gmail.com>
2025-08-21 08:31:28 +0900

    conflict

---
+++ VoiceMemo/Core/Extension/Int+Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Int+Extensions.swift
+//  VoiceMemo
+//
+//  Created by 현유진 on 8/19/25.
+//
+
+import Foundation
+
+extension Int {
+//  var formattedTimeString: String {
+//    let time = Time.fromSeconds(self)
+//    let hoursString = String(format: "%02d", time.hours)
+//    let minutesString = String(format: "%02d", time.minutes)
+//    let secondsString = String(format: "%02d", time.seconds)
+//    
+//    return "\(hoursString) : \(minutesString) : \(secondsString)"
+//  }
+//  
+//  var formattedSettingTime: String {
+//    let currentDate = Date()
+//    /// TimeInterval: 초 단위의 시간 간격을 Double로 나타내준다.
+//    /// addingTimeInterval: 현 시점을 기준으로 현재 날짜와 시간에 지정된 시간을 더해서 새로운 Date()를 반환하는 메서드
+//    let settingDate = currentDate.addingTimeInterval(TimeInterval(self))
+//    
+//    let formatter = DateFormatter()
+//    formatter.locale = Locale(identifier: "ko_KR")
+//    formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+//    formatter.dateFormat = "HH:mm"
+//    
+//    return formatter.string(from: settingDate)
+//  }
+}
